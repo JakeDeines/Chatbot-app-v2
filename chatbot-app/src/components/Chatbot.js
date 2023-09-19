9f78ad55dec0fc009b12147a1362eccc355c1974
JakeDeines <67669417+JakeDeines@users.noreply.github.com>
2023-09-19 14:17:13 -0700

    first commit

---
+++ chatbot-app/src/components/Chatbot.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import OpenAi from 'openai';
+
+const openai = new OpenAi({
+    key: 'Your_API_Key' ,
+
+});
+
+function Chatbot() {
+    // Components Loginc will go here
+}
+
+export default Chatbot;
